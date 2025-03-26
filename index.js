dfde9c13ac1dae78026215f931063d9e8eb83f42
tinamanga <christinamanga28@gmail.com>
2025-03-26 14:34:53 +0300

    Understand why DOMContentLoaded is important

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the paragraph element with id="text"
+    const paragraph = document.getElementById("text");
+  
+    // Replace the text content of the paragraph
+    paragraph.textContent = "This is really cool!";
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+  
